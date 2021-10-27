a13926d1cce3d1334ef3682e93831579c5ea3726
arctumn <pedro.lopes@latitudde.com>
2021-10-27 10:08:52 +0100

    last updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { MessageBoard, User, Message } from './Message';
 
+const pedro:User = {
+  nickname: "Pedro"
+}
+const carlos:User = {
+  nickname: "Carlos"
+}
+const joao:User = {
+  nickname: "Carlos"
+}
+const first_test:Message = {
+  from_user:pedro,
+  private_message:false,
+  message:"Hello World!"
+}
+const second:Message = {
+  from_user:carlos,
+  private_message:false,
+  message:"Goodbye World!"
+}
+const third:Message = {
+  from_user:carlos,
+  private_message:true,
+  to_user: joao,
+  message:"Não curto do Pedro!"
+}
 function App() {
+  const [visable_messages] = useState([first_test,second,third])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MessageBoard {...{message_list:visable_messages,this_user:pedro}} />
   );
 }
 
